dbfe9d349ff5d5403d94adb3f377173ce5860be6
Henry Liu <87534839+henrybliu@users.noreply.github.com>
2023-05-05 03:05:07 -0700

    Update explore.js

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,42 @@
 // explore.js
 
 window.addEventListener('DOMContentLoaded', init);
+const synth = window.speechSynthesis;
+const inputTxt = document.getElementById('text-to-speak');
+const voiceSelect = document.querySelector('select');
+const play = document.querySelector('button');
+const smile = document.querySelector('img');
+let voices;
 
 function init() {
-  // TODO
+  //load voices in
+  if ('onvoiceschanged' in synth) {
+    synth.onvoiceschanged = loadVoices;
+  } else {
+    loadVoices();
+  }
+  //give functionality to the play button
+  play.addEventListener('click', (e) => {
+    //update the face of the face
+    smile.src = 'assets/images/smiling-open.png';
+    const utterThis = new SpeechSynthesisUtterance(inputTxt.value);
+    utterThis.voice = voices[voiceSelect.value];
+    synth.speak(utterThis);
+    inputTxt.blur();
+
+    //revert back the face when utterThis is over
+    utterThis.addEventListener('end', (e) => {
+      smile.src = 'assets/images/smiling.png';
+    });
+  });
+}
+
+function loadVoices() {
+  voices = synth.getVoices();
+  for (let i = 0; i < voices.length; i++) {
+    const option = document.createElement('option');
+    option.textContent = `${voices[i].name} (${voices[i].lang})`;
+    option.value = i;
+    voiceSelect.appendChild(option);
+  }
 }
